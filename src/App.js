119ad83fa46da345048ee20af190debb6c1fa0a9
TARUN KUMAR UTTAM <57785497+zastixx@users.noreply.github.com>
2024-05-14 18:43:41 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import IframeContainer from './IframeContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <IframeContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/IframeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './IframeContainer.css'; // Assuming you move your CSS to a separate file
+
+const IframeContainer = () => {
+  return (
+    <div className="iframe-container">
+      <iframe src="https://zastixx-vx1000.hf.space/" frameborder="0"></iframe>
+    </div>
+  );
+};
+
+export default IframeContainer;
